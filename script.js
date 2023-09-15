fef91d7b185e7cc391548c6f9ce938d2cefa9065
spjyotiranjan <jyotiranjan.sahoo@kalvium.community>
2023-09-15 15:19:00 +0530

    Done

--- script.js
+++ script.js
@@ -6,30 +6,33 @@ const ageContainer = document.getElementById('age-container');
 // Create a variable using the keyword "const", with an identifier as "myName" and assign your name as its value.
 
 // your code goes here
+const myName = "S P Jyotiranjan Sahoo"
+
 
 // Do not edit the below code, uncomment the below line when you are working on challenge 1
-// nameContainer.innerHTML = myName;
+nameContainer.innerHTML = myName;
 
 // Challenge 2: Create a variable with the identifier "myAge" using the appropriate keyword between "const and var" keep one thing in your mind, your age keeps changing, so while declaring the variable ensure that the keyword alows you to change the value of the myAge variable after your birthday.
 
 // your code goes here
+var myAge = 18
 
 // Do not edit the below code, uncomment the below line when you are working on challenge 2
-// ageContainer.innerHTML = myAge;
+ageContainer.innerHTML = myAge;
 
 // Challenge 3: This is a debugging challenge. You can find a few variable declarations below, some of which are correct, and some have a few bugs. Try fixing them. Please look at the console to find the bugs. Uncomment the below lines of code one by one while working on this challenge.
 
-// const 1Question="This is the first debugging question. If you fix this question, you will be able to see this text on your console";
-// console.log(1Question);
+const Question="This is the first debugging question. If you fix this question, you will be able to see this text on your console";
+console.log(Question);
 
-// const #secondQuestion="This is the second debugging question, if you fix this question you will be able to see this text on your console";
-// console.log(#secondQuestion);
+const secondQuestion="This is the second debugging question, if you fix this question you will be able to see this text on your console";
+console.log(secondQuestion);
 
-// const $thirdQuestion="This is the third debugging question, if you fix this question you will be able to see this text on your console";
-// console.log($ThirdQuestion);
+const thirdQuestion="This is the third debugging question, if you fix this question you will be able to see this text on your console";
+console.log(thirdQuestion);
 
-// const _fourthQuestion="This is the fourth debugging question, if you fix this question you will be able to see this text on your console";
-// console.log(_fourthquestion);
+const _fourthQuestion="This is the fourth debugging question, if you fix this question you will be able to see this text on your console";
+console.log(_fourthQuestion);
 
-// const fifth-question="This is the fifth debugging question, if you fix this question you will be able to see this text on your console";
-// console.log(fifth-question);
+const fifthquestion="This is the fifth debugging question, if you fix this question you will be able to see this text on your console";
+console.log(fifthquestion);
